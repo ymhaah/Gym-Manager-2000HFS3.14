a67dfb4715f7c800e65950534fdf0d585c8e34eb
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2023-10-03 01:16:22 +0300

    add Next Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
+import react from "@vitejs/plugin-react-swc";
+import { Button, ButtonGroup } from "@nextui-org/react";
 
-function App() {
-  // const [count, setCount] = useState(0)
+function App({ name }: { name: string }) {
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <h1>
+                {name} are {count}
+                <Button color="primary">Button</Button>
+            </h1>
+        </>
+    );
 }
 
-export default App
+export default App;
